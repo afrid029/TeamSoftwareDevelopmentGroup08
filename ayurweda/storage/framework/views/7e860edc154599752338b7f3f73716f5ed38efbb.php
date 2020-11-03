d941f9c80ce94d67c5a4153d6d85142fa5e692e9
afrid029 <mafrid029@gmail.com>
2020-11-03 16:39:24 +0530

    vendor added

---
+++ ayurweda/storage/framework/views/7e860edc154599752338b7f3f73716f5ed38efbb.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <title>Login</title>
+
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="<?php echo e(asset('css/bootstrap.min.css')); ?>">
+     <link rel="stylesheet" href="<?php echo e(asset('css/font-awesome.min.css')); ?>">
+     <link rel="stylesheet" href="<?php echo e(asset('css/animate.css')); ?>">
+     <link rel="stylesheet" href="<?php echo e(asset('css/owl.carousel.css')); ?>">
+     <link rel="stylesheet" href="<?php echo e(asset('css/owl.theme.default.min.css')); ?>">
+     <link rel="stylesheet" href="<?php echo e(asset('css/magnific-popup.css')); ?>">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="<?php echo e(asset('css/login.css')); ?>">
+
+</head>
+<body>
+
+     <!-- PRE LOADER -->
+     <section class="preloader">
+          <div class="spinner">
+
+               <span class="spinner-rotate"></span>
+               
+          </div>
+     </section>
+
+
+     <!-- MENU -->
+     <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+
+                    <!-- lOGO TEXT HERE -->
+                    <a href="welcome" class="navbar-brand">Hospital <span>.</span> Pharmacy</a>
+               </div>
+
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    
+
+                    <ul class="nav navbar-nav navbar-right">
+                         <li><a href="#">Call Now! <i class="fa fa-phone"></i> 010 020 0340</a></li>
+                    </ul>
+               </div>
+
+          </div>
+     </section>
+
+
+     <!-- HOME -->
+     <section id="home" class="slider" data-stellar-background-ratio="0.5">
+          <div class="row">
+
+                    <div class="owl-carousel owl-theme">
+                         <div class="item item-first">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-8 col-sm-12">
+                                            <form action="log" method="post" class="wow fadeInUp"> 
+                                             <h1>Login</h1><br><br>
+                                             <div class="col-md-6 col-sm-6">
+                                             <input class="form-control" type="text" name="username" placeholder="Username"><br>
+                                             <input class="form-control" type="password" name="password" placeholder="Password"><br><br>
+                                             <input class="section-btn btn btn-default smoothScroll" type="submit" value="Login" color="black">
+                                             </div>
+                                             </form>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                         
+                    </div>
+
+          </div>
+     </section>
+
+
+     <!-- SCRIPTS -->
+     <script src="<?php echo e(asset('js/jquery.js')); ?>"></script>
+     <script src="<?php echo e(asset('js/bootstrap.min.js')); ?>"></script>
+     <script src="<?php echo e(asset('js/jquery.stellar.min.js')); ?>"></script>
+     <script src="<?php echo e(asset('js/wow.min.js')); ?>"></script>
+     <script src="<?php echo e(asset('js/owl.carousel.min.js')); ?>"></script>
+     <script src="<?php echo e(asset('js/jquery.magnific-popup.min.js')); ?>"></script>
+     <script src="<?php echo e(asset('js/smoothscroll.js')); ?>"></script>
+     <script src="<?php echo e(asset('js/custom.js')); ?>"></script>
+
+</body>
+</html><?php /**PATH C:\Users\AJANTHA\Desktop\TeamSoftwareDevelopmentGroup08\ayurweda\resources\views/login.blade.php ENDPATH**/ ?>
---
+++ ayurweda/storage/framework/views/89bd675d87bc9e8f02cfec2ab7c6fc8ae50751fe.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="log" method="post" class="wow fadeInUp"> 
+                                             <h1>Register</h1><br><br>
+                                             <div class="col-md-6 col-sm-6">
+                                             <select id="catagory" name="catagory" onChange="GFG_Fun()" class="form-control">
+                                             <option selected="true" disabled="disabled">Choose Catagory</option>
+                                             <option value="doctor">Doctor</option>
+                                             <option value="patient">Patient</option>
+                                             <option value="pharmacist">Pharmacist</option>
+                                             <option value="producer">Medicine Producer</option>
+                                             <option value="supplier">Ingredients supplier</option>
+                                             </select>
+                                             <script src="<?php echo e(asset('js/form.js')); ?>"></script>
+                                             <input class="form-control" type="text" name="username" placeholder="Username"><br>
+                                             <input class="form-control" type="password" name="password" placeholder="Password"><br><br>
+                                             <input class="section-btn btn btn-default smoothScroll" type="submit" value="Register" color="black">
+                                             </div>
+                                             </form>
+</body>
+</html><?php /**PATH C:\Users\AJANTHA\Desktop\TeamSoftwareDevelopmentGroup08\ayurweda\resources\views/test.blade.php ENDPATH**/ ?>
